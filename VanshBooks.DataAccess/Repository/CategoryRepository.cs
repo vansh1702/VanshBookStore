e8617d55726b7f89ad4ac76de453c84139770aca
W0827163 <W0827163@ADL-A1201I-18>
2023-11-06 18:05:13 -0500

    Modified CategoryRepository.cs

---
+++ VanshBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using VanshBooks.DataAccess.Repository.IRepository;
+using VanshBooks.Models;
+using VanshBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VanshBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ VanshBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VanshBooks.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository                       //removed class
+    {
+    }
+}
